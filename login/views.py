0496d2aaed64c701d2700f545aa569a49d8bafba
xue-663 <593137303@qqcom>
2019-07-24 18:05:36 +0800

    Initial commit

---
+++ login/views.py
@@ -0,0 +1,158 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from . import models
+from . import form
+from django.conf import settings
+
+import hashlib
+import datetime
+
+def send_email(email, code):
+
+    from django.core.mail import EmailMultiAlternatives
+
+    subject = '来自xuebin的注册确认邮件'
+
+    text_content = '''感谢注册www.yyyy.com，这里是一个xuebin的登录操作系统，祝您使用愉快喔！！！
+    O(∩_∩)O谢谢！'''
+
+    html_content = '''
+                    <p>感谢注册<a href="http://{}/confirm/?code={}" target=blank>www.xxxx.com</a>，\
+                    这里是一个登录操作系统，O(∩_∩)O谢谢！</p>
+                    <p>请点击站点链接完成注册确认！</p>
+                    <p>此链接有效期为{}天！</p>
+                    '''.format('127.0.0.1:8000', code, settings.CONFIRM_DAYS)
+
+    msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, [email])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+def make_confirm_string(user):
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    code = hash_code(user.name, now)
+    models.ConfirmString.objects.create(code=code, user=user,)
+    return code
+def user_confirm(request):
+    code = request.GET.get('code', None)
+    message = ''
+    try:
+        confirm = models.ConfirmString.objects.get(code=code)
+    except:
+        message = '无效的确认请求!'
+        return render(request, 'login/confirm.html', locals())
+
+    c_time = confirm.c_time
+    now = datetime.datetime.now()
+    if now > c_time + datetime.timedelta(settings.CONFIRM_DAYS):
+        confirm.user.delete()
+        message = '您的邮件已经过期！请重新注册!'
+        return render(request, 'login/confirm.html', locals())
+    else:
+        confirm.user.has_confirmed = True
+        confirm.user.save()
+        confirm.delete()
+        message = '感谢确认，请使用账户登录！'
+        return render(request, 'login/confirm.html', locals())
+
+def hash_code(s, salt='mysite'):# 加点盐
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())  # update方法只接收bytes类型
+    return h.hexdigest()
+
+# Create your views here.
+def index(request):
+    if not request.session.get('is_login', None):
+        return redirect('/login/')
+    return render(request, 'login/index.html')
+
+
+def login(request):
+    if request.session.get('is_login', None):  # 不允许重复登录
+        return redirect('/index/')
+    if request.method == 'POST':
+        login_form = form.UserForm(request.POST)
+        message = '请检查填写的内容！'
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+
+            try:
+                user = models.User.objects.get(name=username)
+            except :
+                message = '用户不存在！'
+                return render(request, 'login/login.html', locals())
+
+            if not user.has_confirmed:
+                message = '该用户还未经过邮件确认！'
+                return render(request, 'login/login.html', locals())
+
+            if user.password == hash_code(password):
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
+                return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'login/login.html', locals())
+        else:
+            return render(request, 'login/login.html', locals())
+
+    login_form = form.UserForm()
+    return render(request, 'login/login.html', locals())
+
+def register(request):
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+
+    if request.method == 'POST':
+        register_form = form.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data.get('username')
+            password1 = register_form.cleaned_data.get('password1')
+            password2 = register_form.cleaned_data.get('password2')
+            email = register_form.cleaned_data.get('email')
+            sex = register_form.cleaned_data.get('sex')
+
+            if password1 != password2:
+                message = '两次输入的密码不同！'
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:
+                    message = '用户名已经存在'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = '该邮箱已经被注册了！'
+                    return render(request, 'login/register.html', locals())
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = hash_code(password1)
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+
+                code = make_confirm_string(new_user)
+                send_email(email, code)
+
+                message = '请前往邮箱进行确认！'
+                return render(request, 'login/confirm.html', locals())
+        else:
+            return render(request, 'login/register.html', locals())
+    register_form = form.RegisterForm()
+    return render(request, 'login/register.html', locals())
+
+
+def logout(request):
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/login/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    return redirect("/login/")
---
+++ send_mail.py
@@ -0,0 +1,13 @@
+# import os
+# from django.core.mail import send_mail
+#
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'xiangmu.settings'
+#
+# if __name__ == '__main__':
+#
+#     send_mail(
+#         '来自xuebin的测试邮件',
+#         '欢迎光临，欢迎和我一起学习Django',
+#         '593137303@qq.com',
+#         ['286665119@qq.com'],
+#     )
